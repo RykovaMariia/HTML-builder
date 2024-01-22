10a5c77262c968d9cff266c5b0b9cad6d910122e
Mariia Rykova <maryrykova24@gmail.com>
2024-01-22 18:58:54 +0400

    refactor: refactor the 6th task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,113 +1,130 @@
 const fsp = require('fs/promises');
 const path = require('path');
 
-let pathFolderAssets = path.join(__dirname, 'assets');
-let pathFolderStyles = path.join(__dirname, 'styles');
-let pathHTML = path.join(__dirname, 'template.html');
-let pathComponents = path.join(__dirname, 'components');
-
-let pathFolderDist = path.join(__dirname, 'project-dist');
-let pathFolderDistAssets = path.join(__dirname, 'project-dist', 'assets');
-let pathFileDistStyle = path.join(__dirname, 'project-dist', 'style.css');
-let pathFileDistHTML = path.join(__dirname, 'project-dist', 'index.html');
-
 (async () => {
-  await checkExistAndRemove(pathFolderDist);
-  await fsp
-    .mkdir(pathFolderDist, { recursive: true })
-    .then(() => {
-      createAssetsAndCopy(pathFolderDistAssets);
-      createStyleAndMerge();
-      mergeHTML();
-    })
-    .catch((error) => console.log(error));
+  try {
+    const pathFolderDist = path.join(__dirname, 'project-dist');
+    const pathFolderDistAssets = path.join(__dirname, 'project-dist', 'assets');
+
+    await checkExistAndRemove(pathFolderDist);
+    await fsp.mkdir(pathFolderDist, { recursive: true });
+    await createAssetsAndCopy(pathFolderDistAssets);
+    await createStyleAndMerge();
+    await mergeHTML();
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
-async function checkExistAndRemove(pathFolderDist) {
-  const isFolderDistExists = await fsp
-    .access(pathFolderDist)
-    .then(() => true)
-    .catch(() => false);
+async function checkExistAndRemove(pathFolder) {
+  try {
+    const isFolderDistExists = await fsp
+      .access(pathFolder)
+      .then(() => true)
+      .catch(() => false);
 
-  if (isFolderDistExists) {
-    await fsp
-      .rm(pathFolderDist, { recursive: true }, { force: true })
-      .then(() => {})
-      .catch((error) => console.log(error));
+    if (isFolderDistExists) {
+      await fsp.rm(pathFolder, { recursive: true }, { force: true });
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
 
-async function createAssetsAndCopy() {
-  await checkExistAndRemove(pathFolderDistAssets);
-  await fsp
-    .mkdir(pathFolderDistAssets, { recursive: true })
-    .then(() => {
-      copyAssets(pathFolderAssets, pathFolderDistAssets);
-    })
-    .catch((error) => console.log(error));
+async function createAssetsAndCopy(pathFolderDistAssets) {
+  try {
+    const pathFolderAssets = path.join(__dirname, 'assets');
+
+    await checkExistAndRemove(pathFolderDistAssets);
+    await fsp.mkdir(pathFolderDistAssets, { recursive: true });
+    await copyAssets(pathFolderAssets, pathFolderDistAssets);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function copyAssets(pathFolder, pathFolderCopy) {
-  let data = await fsp.readdir(pathFolder, {
-    withFileTypes: true,
-  });
+  try {
+    const filesFolder = await fsp.readdir(pathFolder, { withFileTypes: true });
 
-  for (let file of data) {
-    const pathFile = path.join(pathFolder, file.name);
-    const pathFileCopy = path.join(pathFolderCopy, file.name);
+    for (let file of filesFolder) {
+      const pathFile = path.join(pathFolder, file.name);
+      const pathFileCopy = path.join(pathFolderCopy, file.name);
 
-    if (file.isFile()) {
-      await fsp.copyFile(pathFile, pathFileCopy).then(() => {});
-    } else if (file.isDirectory()) {
-      fsp
-        .mkdir(path.join(pathFolderCopy, file.name), { recursive: true })
-        .then(() => {
-          copyAssets(
-            path.join(pathFolder, file.name),
-            path.join(pathFolderCopy, file.name),
-          );
-        })
-        .catch((error) => console.log(error));
+      if (file.isFile()) {
+        await fsp.copyFile(pathFile, pathFileCopy);
+      } else if (file.isDirectory()) {
+        await fsp.mkdir(path.join(pathFolderCopy, file.name), {
+          recursive: true,
+        });
+        await copyAssets(
+          path.join(pathFolder, file.name),
+          path.join(pathFolderCopy, file.name),
+        );
+      }
     }
+  } catch (err) {
+    console.log(err);
   }
 }
 
 async function createStyleAndMerge() {
-  await checkExistAndRemove(pathFileDistStyle);
-  await mergeStyle();
+  try {
+    const pathFileDistStyle = path.join(__dirname, 'project-dist', 'style.css');
+    await checkExistAndRemove(pathFileDistStyle);
+    await mergeStyle(pathFileDistStyle);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function mergeStyle() {
-  await fsp.readdir(pathFolderStyles, { withFileTypes: true }).then((data) => {
-    for (let file of data) {
+async function mergeStyle(pathFileDistStyle) {
+  try {
+    const pathFolderStyles = path.join(__dirname, 'styles');
+    const filesStyle = await fsp.readdir(pathFolderStyles, {
+      withFileTypes: true,
+    });
+
+    for (let file of filesStyle) {
       const pathFile = path.join(pathFolderStyles, file.name);
       const fileExt = path.extname(pathFile);
 
       if (file.isFile() && fileExt === '.css') {
-        fsp.readFile(pathFile, 'utf-8').then((dataFile) => {
-          fsp.appendFile(pathFileDistStyle, dataFile);
-        });
+        const dataFile = await fsp.readFile(pathFile, 'utf-8');
+        await fsp.appendFile(pathFileDistStyle, dataFile);
       }
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function mergeHTML() {
-  checkExistAndRemove(pathFileDistHTML);
-  const data = await fsp.readFile(pathHTML, { encoding: 'utf8' });
-  let dataHTML = data;
+  try {
+    let pathFileDistHTML = path.join(__dirname, 'project-dist', 'index.html');
+    let pathHTML = path.join(__dirname, 'template.html');
+    let pathComponents = path.join(__dirname, 'components');
 
-  const components = await fsp.readdir(pathComponents, { withFileTypes: true });
+    await checkExistAndRemove(pathFileDistHTML);
 
-  for (let component of components) {
-    const pathComponent = path.join(pathComponents, component.name);
-    const componentExt = path.extname(pathComponent);
+    let data = await fsp.readFile(pathHTML, { encoding: 'utf8' });
 
-    if (component.isFile() && componentExt === '.html') {
-      const componentName = component.name.split('.')[0];
-      const componentData = await fsp.readFile(pathComponent, 'utf-8');
-      dataHTML = dataHTML.replace(`{{${componentName}}}`, componentData);
-      await fsp.writeFile(pathFileDistHTML, dataHTML);
+    const components = await fsp.readdir(pathComponents, {
+      withFileTypes: true,
+    });
+
+    for (let component of components) {
+      const pathComponent = path.join(pathComponents, component.name);
+      const componentExt = path.extname(pathComponent);
+
+      if (component.isFile() && componentExt === '.html') {
+        const componentName = component.name.split('.')[0];
+        const componentData = await fsp.readFile(pathComponent, 'utf-8');
+        data = data.replace(`{{${componentName}}}`, componentData);
+        await fsp.writeFile(pathFileDistHTML, data);
+      }
     }
+  } catch (err) {
+    console.log(err);
   }
 }
