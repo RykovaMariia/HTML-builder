c0740482abf052a32167c6d12b42dc2e01664c4c
Mariia Rykova <maryrykova24@gmail.com>
2024-01-22 16:27:00 +0400

    refactor: refactor the 5th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,35 +1,41 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const pathFolder = path.join(__dirname, 'styles');
-const pathFileProject = path.join(__dirname, 'project-dist', 'bundle.css');
-
 (async () => {
-  const isFileExists = await fs
-    .access(pathFileProject)
-    .then(() => true)
-    .catch(() => false);
+  try {
+    const pathFileProject = path.join(__dirname, 'project-dist', 'bundle.css');
 
-  if (isFileExists) {
-    await fs
-      .rm(pathFileProject, { recursive: true }, { force: true })
-      .then(() => console.log('bundle.css was deleted'))
-      .catch((error) => console.log(error));
+    const isFileProjectExists = await fs
+      .access(pathFileProject)
+      .then(() => true)
+      .catch(() => false);
+
+    if (isFileProjectExists) {
+      await fs.rm(pathFileProject, { recursive: true }, { force: true });
+    }
+    await mergeStyle(pathFileProject);
+  } catch (err) {
+    console.log(err);
   }
-  mergeStyle();
 })();
 
-async function mergeStyle() {
-  await fs.readdir(pathFolder, { withFileTypes: true }).then((data) => {
-    for (let file of data) {
+async function mergeStyle(pathFileProject) {
+  try {
+    const pathFolder = path.join(__dirname, 'styles');
+
+    const folderData = await fs.readdir(pathFolder, { withFileTypes: true });
+
+    for (let file of folderData) {
       const pathFile = path.join(pathFolder, file.name);
       const fileExt = path.extname(pathFile);
 
       if (file.isFile() && fileExt === '.css') {
-        fs.readFile(pathFile, 'utf-8').then((dataFile) => {
+        await fs.readFile(pathFile, 'utf-8').then((dataFile) => {
           fs.appendFile(pathFileProject, dataFile);
         });
       }
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 }
